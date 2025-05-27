2d81ace7380465fea3682ed8607c37c9e8f4cb7f
Victor Oduor <victoroduorr@gmail.com>
2025-05-27 22:07:18 +0300

    .

--- recap/dataWrangling.py
+++ recap/dataWrangling.py
@@ -29,3 +29,31 @@ df2["price_dollars"] = (df2["price_mxn"]/19).round(2) #assuming 1 dollar = 19 pe
 
 #the we drop the price_mxn column
 df2 = df2.drop(column =["price_mxn"], inplace=True)
+
+
+
+
+###fixing Df3, we will remove NaN, and extract the feature "state" from the column "place_with_parent_names"
+###likewise, we have a column with both latitude and longitude features put together, so we need to seperate them.
+
+#drop NaN values
+df3 = df3.dropna(inplace=True)
+
+#seperate the "lat" and "lon" features from the "lat-lon" column
+
+df3[["lat", "lon"]] = df3['lat-lon'].str.split(',', expand=True) # expand = True tells pandas to treat the splitted items as columns
+
+#extract "state" from "place_with_parent_Names"
+
+df3["state"] = df3["place_with_parent_names"].str.split('|', expand = True)[2]
+
+# we need to drop the nolonger important columns "lat-lon" and "place_with_parent_names" 
+
+df3.drop(columns =["place_with_parent_names", "lat-lon"], inplace= True)
+
+
+
+###concatenating the 3 dataframes to form 1....
+# this shall be done row-wise, meaning axis = 0, usually that way by default
+
+df = pd.concat([df1, df2, df3])
